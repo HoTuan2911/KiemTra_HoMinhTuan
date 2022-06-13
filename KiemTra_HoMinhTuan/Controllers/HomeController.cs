02d7a4c6c18d1353d55083d6e232f3cbcd0a01f0
HoTuan2911 <darkboy2911@gmail.com>
2022-06-13 15:13:48 +0700

    update lan 1

--- KiemTra_HoMinhTuan/Controllers/HomeController.cs
+++ KiemTra_HoMinhTuan/Controllers/HomeController.cs
@@ -16,6 +16,41 @@ namespace KiemTra_HoMinhTuan.Controllers
             return View(all_sinhvien);
         }
 
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SinhVien sv)
+        {
+            var E_MaSV = collection["MaSV"];
+            var E_HoTen = collection["HoTen"];
+            var E_GioiTinh = collection["GioiTinh"];
+            var E_NgaySinh = Convert.ToDateTime(collection["NgaySinh"]);
+            var E_Hinh = collection["Hinh"];
+            var E_MaNganh = collection["MaNganh"];
+            if (string.IsNullOrEmpty(E_HoTen))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                sv.MaSV = E_MaSV;
+                sv.HoTen = E_HoTen.ToString();
+                sv.GioiTinh = E_GioiTinh.ToString();
+                sv.NgaySinh = E_NgaySinh;
+                sv.Hinh = E_Hinh.ToString();
+                sv.MaNganh = E_MaNganh.ToString();
+                data.SinhViens.InsertOnSubmit(sv);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
