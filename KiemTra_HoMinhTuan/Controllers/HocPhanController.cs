95eac8e7cd15e916f9210a2bbadf56ab21bcad23
HoTuan2911 <darkboy2911@gmail.com>
2022-06-13 16:05:49 +0700

    update lan 5

---
+++ KiemTra_HoMinhTuan/Controllers/HocPhanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KiemTra_HoMinhTuan.Models;
+
+namespace KiemTra_HoMinhTuan.Controllers
+{
+    public class HocPhanController : Controller
+    {
+
+        MyDataDataContext data = new MyDataDataContext();
+        // GET: HocPhan
+        public ActionResult ListHocPhan()
+        {
+            var all_HocPhan = from hp in data.HocPhans select hp;
+            return View(all_HocPhan);
+        }
+    }
+}
