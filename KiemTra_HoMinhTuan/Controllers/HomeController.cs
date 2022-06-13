5bb8f5e57527aad6d969c3d6ba698e2f0f840c94
HoTuan2911 <darkboy2911@gmail.com>
2022-06-13 14:46:36 +0700

    Index

--- KiemTra_HoMinhTuan/Controllers/HomeController.cs
+++ KiemTra_HoMinhTuan/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KiemTra_HoMinhTuan.Models;
 
 namespace KiemTra_HoMinhTuan.Controllers
 {
     public class HomeController : Controller
     {
+        MyDataDataContext data = new MyDataDataContext();
         public ActionResult Index()
         {
-            return View();
+            var all_sinhvien = from ss in data.SinhViens select ss;
+            return View(all_sinhvien);
         }
 
         public ActionResult About()
